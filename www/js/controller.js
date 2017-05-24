2ca3a648a58299016261303e98e61e8dbb06c005
Christian Acuna <christian_acuna0112@yahoo.com>
2017-05-24 15:35:13 +0800

    edited controller.js and tabs-contacts.html

--- www/js/controller.js
+++ www/js/controller.js
@@ -3,22 +3,25 @@ angular.module('ionicApp.controllers', [])
 .controller('signupCtrl', function($scope, $state, $rootScope, LoginService, $ionicPopup) {
     $scope.data = {}
  
-    $scope.phone = function(data) {
+    $scope.logIn = function() {
+        LoginService.loginUser($scope.data.username, $scope.data.password).success(function(data) {
+            $state.go('tabs.contacts')
+            console.log('Login Successfully')
+        })
+            .error(function(data) {
+                console.log('Login Failed')
+                var alertPopup = $ionicPopup.alert({
+                title: 'Login failed!',
+                template: '<h4 style="text-align: center">Please Try Again!!!</h4>'            
+        })
+      })
+   }
+     $scope.phone = function(data) {
         $state.go('phone')
         console.log('Enter Phone Number')
     }
-
-    $scope.login = function() {
-        LoginService.loginUser($scope.data.username, $scope.data.password).success(function(data) {
-            $state.go('tab.contacts');
-        }).error(function(data) {
-            var alertPopup = $ionicPopup.alert({
-                title: 'Login failed!',
-                template: 'Wrong User Name or Password!'
-            });
-        });
-    }
 })
+
 
 .controller('phoneCtrl', function($scope, $state) {
     $scope.valid = function(data) {
