5af0937f263143144d6514ad12698d02752c4254
Alyssa Calip <calipalyssamae@gmail.com>
2017-05-24 15:24:14 -0700

    added

--- www/js/controller.js
+++ www/js/controller.js
@@ -2,7 +2,17 @@ angular.module('ionicApp.controllers', [])
 
 .controller('signupCtrl', function($scope, $state, $rootScope, LoginService, $ionicPopup) {
     $scope.data = {}
- 
+   $scope.logIn = function() {
+        LoginService.loginUser($scope.data.username, $scope.data.password).success(function(data) {
+            $state.go('tabs')
+        }).error(function(data) {
+            var alertPopup = $ionicPopup.alert({
+                title: 'Login failed!',
+                template: 'Please check your credentials!'
+            });
+        });
+    }
+
     $scope.phone = function(data) {
         $state.go('phone')
         console.log('Enter Phone Number')
